a0c61b3d3b68691c3ed58307508f051187c7776e
hhpxme <hhpxme@gmail.com>
2023-11-08 09:01:36 +0700

    animation page

---
+++ P6_0111_AnimationLandingPage/js/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle('dark');
+});
+
+
+
